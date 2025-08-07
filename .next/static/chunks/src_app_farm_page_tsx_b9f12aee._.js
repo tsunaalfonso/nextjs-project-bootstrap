82e445044b4e5d36ee0b3022985306c713c1bc91
KellinQuinn01 <47975483+tsunaalfonso@users.noreply.github.com>
2025-08-08 02:54:11 +0800

    Add .next/static/chunks/src_app_farm_page_tsx_b9f12aee._.js 1754592850040

---
+++ .next/static/chunks/src_app_farm_page_tsx_b9f12aee._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_24211c3a._.js",
+  "static/chunks/src_c6fea1e4._.js"
+],
+    source: "dynamic"
+});
