2ea47be440dbb0e8189d93a25b95592c01c72246
KellinQuinn01 <47975483+tsunaalfonso@users.noreply.github.com>
2025-08-08 02:54:26 +0800

    Add .next/static/chunks/src_app_restaurant_page_tsx_b9f12aee._.js 1754592865039

---
+++ .next/static/chunks/src_app_restaurant_page_tsx_b9f12aee._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_6ad1c264._.js",
+  "static/chunks/src_7c3fe7cd._.js"
+],
+    source: "dynamic"
+});
